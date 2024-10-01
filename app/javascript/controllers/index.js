78e0908cca8d502d141ce92a33b1bd3aefe86232
Christopher Lim <toffer.lim87@gmail.com>
2024-09-30 22:57:25 -0700

    - update the submission form - add tab controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,6 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import TabsController from "./tabs_controller"
+application.register("tabs", TabsController)
 eagerLoadControllersFrom("controllers", application)
