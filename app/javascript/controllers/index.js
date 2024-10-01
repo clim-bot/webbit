427448f91982c8b37a0cae9637c250d79de346d4
Christopher Lim <toffer.lim87@gmail.com>
2024-09-30 23:51:38 -0700

    - fix the menu for closing and opening

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,4 +3,6 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import TabsController from "./tabs_controller"
 application.register("tabs", TabsController)
+import DropdownController from "./dropdown_controller"
+application.register("dropdown", DropdownController)
 eagerLoadControllersFrom("controllers", application)
