5faf990ab89d801d1e27477e45b3b221256ca390
Christopher Lim <toffer.lim87@gmail.com>
2024-10-05 22:26:00 -0700

    - addong comments

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :submission
+  belongs_to :user
+
+  validates :reply, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   resources :communities
-  resources :submissions
+
+  resources :submissions do
+    member do
+      put "upvote", to: "submissions#upvote"
+      put "downvote", to: "submissions#downvote"
+    end
+
+    resources :comments do
+      member do
+        put "upvote", to: "comments#upvote"
+        put "downvote", to: "comments#downvote"
+      end
+    end
+  end
+
   devise_for :users
   root "submissions#index"
 end
